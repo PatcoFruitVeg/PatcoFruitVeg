59a13fe05eeadd477ffe50a6e8e6ecf11b6081ce
Parin Parikh <parin_parikh@yahoo.co.uk>
2014-08-03 17:03:58 +0100

    JSHint and CSSLint errors fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* jshint ignore:start */
 (function(i,s,o,g,r,a,m){i.GoogleAnalyticsObject=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,6 +8,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-53461628-1', 'auto');
 ga('send', 'pageview');
+/* jshint ignore:end */
 
 $('body').scrollspy({ target: '#bs-example-navbar-collapse-1', offset: 110 });
 
