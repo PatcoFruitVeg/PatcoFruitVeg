698d34ddaaff3320c6f155ce45080971744eeabb
Parin Parikh <parin_parikh@yahoo.co.uk>
2014-08-15 19:38:04 +0100

    Loading content better

--- js/newMain.js
+++ js/newMain.js
@@ -1,9 +1,17 @@
 'use strict';
 
 $(function() {
-  var logo = $('#logo');
-  var contact = $('#contact');
+  var background = new Image();
+  background.src = '/img/bg.png';
 
-  logo.addClass('animated bounceIn');
-  contact.addClass('animated fadeIn');
+  background.onload = function() {
+    $('body').addClass('cover');
+    $('.container').removeClass('none');
+
+    var logo = $('#logo');
+    var contact = $('#contact');
+
+    logo.addClass('animated bounceIn');
+    contact.addClass('animated fadeIn');
+  };
 });
